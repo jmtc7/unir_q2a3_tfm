ad188d7211e63b0ffb605c848ae1f4c3c5a8ee18
jmtc7 <josemigueltorrescamara@gmail.com>
2026-05-28 18:19:45 +0200

    Reorganizar API para Tesseract

--- ocr/tesseract/app.py
+++ ocr/tesseract/app.py
@@ -9,4 +9,5 @@ app = FastAPI()
 async def ocr(file: UploadFile = File(...)):
     image = Image.open(io.BytesIO(await file.read()))
     text = pytesseract.image_to_string(image, lang="spa")
-    return {"text": text}
+    return {"engine": "Tesseract OCR",
+            "text"  : text}
